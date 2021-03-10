0bde6b148a5ab5ab484c9c2bd2243f8e332f7bce
Gabriela Wielgus <gabwielgus@gmail.com>
2021-03-10 17:07:59 +0100

    Add new files

---
+++ przedszkolanka.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+int nwd(int m, int n) 
+{
+    if (n != 0)
+        return nwd(n, m % n);
+    else
+        return m;
+}
+int nww(int m, int n)
+{
+    return m * n / nwd(m, n);
+}
+int main()
+{
+    int n,a,b;
+    cin >> n;
+    if (n < 1 || n>20) return 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+        if (a < 10 || b < 10 || a> 30 || b>30) return 0;
+        cout << nww(a, b) << endl;
+    }
+    return 0;
+}
+
